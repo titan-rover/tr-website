a26b41afd788ddf2b95f7dc74184a924e966212c
joannsum <joannsmsum@gmail.com>
2024-09-13 14:30:53 -0700

    team structure but not responsive and no images

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,19 @@
+import { SubteamSlider } from '../components/About/SubteamSlider';
+
 export const AboutPage = () => {
-    return (
-        <>
-        <h1>about page</h1>
-        </>
-    )
-}
+  return (
+    <>
+        
+        <div className="px-20">    
+          <h1 className="text-4xl font-bold mt-6 mb-6">Our Team Structure</h1>
+          <div className="px-20">
+          <p className="text-lg mb-6 px-20 text-left">
+          Titan Rover’s team is divided into five specialized subteams—Mechanical, Electrical, Controls, Business, and Life Sciences. Each subteam brings unique expertise, working together to ensure the success of our rover missions.
+          </p>
+          <h1 className="text-4xl font-bold text-left px-20">Subteams</h1>
+          <SubteamSlider />
+          </div>
+      </div>    
+    </>
+  );
+};
