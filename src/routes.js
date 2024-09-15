1b81a38096f4855edd02091a76454bff3172e949
Ola <oyinkansola_o@yahoo.com>
2024-09-14 22:28:38 -0700

    footer desktop mode done

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 import { HomePage } from './pages/HomePage';
 import { AboutPage } from './pages/AboutPage';
 import { AlumniPage } from './pages/AlumniPage';
@@ -10,12 +11,14 @@ const AppRoutes = () => {
     <Router>
       <div>
         <Navbar />
+        
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/alumni" element={<AlumniPage />} />
         </Routes>
       </div>
+      <Footer /> 
     </Router>
   );
 }
