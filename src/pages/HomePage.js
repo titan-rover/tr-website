3359508363cc39d7b0f113736f0947f612ba146a
Ola <oyinkansola_o@yahoo.com>
2024-09-30 15:05:14 -0700

    added video latest video section, dark mode + responsiveness

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,25 @@
 import { WhyJoin } from "../components/About/WhyJoin";
+import YoutubeEmbed from "../YoutubeEmbeded";
 
 export const HomePage = () => {
+	
+
 	return (
 		<>
-			<h1>home page</h1>
+			{/* <h1>home page</h1> */}
 			<WhyJoin />
+
+
+			
+			<div className= "flex flex-col items-center py-8  md:py-5 md:px-10 dark:bg-black"> 
+				<h1 className="text-4xl font-bold py-8 md:py-14 text-center dark:text-white">
+				Check out our latest Video
+				</h1>
+				<YoutubeEmbed embedId="OTfGrkzJBZI" />
+			</div>
 		</>
+
+
+		
 	)
 }
